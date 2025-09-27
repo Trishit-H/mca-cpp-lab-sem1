f3d0c71805150450e01b0da068089f4c866c0c7b
Trishit Hazra <trishitgithub@gmail.com>
2025-09-27 22:20:30 +0530

    Finished Question 3 from Week 5

---
+++ Week 5/3.length_of_array.cpp
@@ -0,0 +1,13 @@
+// Write a C++ program to find the length of the array.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = { 10, 20, 30, 40, 50 };
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "The length of the array is: " << length << endl;
+
+    return 0;
+}
