7f7cf88f508f14e5e47e9d7c0cce4cac44d2b0c7
Trishit Hazra <trishitgithub@gmail.com>
2025-09-27 22:20:09 +0530

    Finished Question 2 from Week 5

---
+++ Week 5/2.sum_of_elements.cpp
@@ -0,0 +1,33 @@
+// Write a C++ program to find the sum of all the array elements.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int sum = 0, arrayLength;
+
+    cout << "Enter length of the array: ";
+    cin >> arrayLength;
+
+    int arr[arrayLength];
+
+    cout << "Enter the array elements: ";
+    for(int i = 0; i < arrayLength; i++) {
+        cin >> arr[i];
+    }
+
+    // Display the array:
+    cout << "Array: ";
+    for(int i = 0; i < arrayLength; i++) {
+        cout << arr[i] << " ";
+    }
+
+    // Calculate the sum
+    for(int i = 0; i < arrayLength; i++) {
+        sum = sum + arr[i];
+    }
+
+    cout << "\nThe sum of all the elements of the array is: " << sum << endl;
+
+    return 0;
+}
